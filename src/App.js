e411fab96d7363a486f294f41504b2781f3a9932
Ubuntu <ubuntu@ip-172-31-36-54.ap-southeast-1.compute.internal>
2021-10-06 14:00:14 +0000

    added daily meme

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './main.scss';
 import './styles.scss';
 import BoardView from './components/BoardView.js';
 import FAQ1 from './assets/img/faq1.jpg';
 import FAQ2 from './assets/img/faq2.jpg';
-import FAQ3 from './assets/img/faq3.jpg';
 import MOE1 from './assets/img/moe1.gif';
 import MOE2 from './assets/img/moe2.gif';
 import MOE3 from './assets/img/moe3.gif';
 
 function App() {
+  const [url, setUrl] = useState("");
+  function fetch_data() {
+    fetch("https://api.imgflip.com/get_memes")
+      .then(
+        (res) => {
+          if (res.ok) {
+            return res.json();
+          }
+          throw new Error("Request Failed");
+        },
+        (networkError) => console.log(networkError.message)
+      )
+      .then((jsonRes) => {
+        const num = Math.floor(Math.random() * 100);
+        console.log("test");
+        console.log(jsonRes.data.memes[num].url);
+        setUrl(jsonRes.data.memes[num].url);
+      });
+  }
+  
   return (
     <div className="white-bg">
       <h1>2ome 0verly 4gitating 8oard GAME</h1>
@@ -34,25 +53,22 @@ function App() {
           </div>
         </div>
       </div>
-      
-      
-      
       <b>FAQs: </b>
-            <br/>
-            <div className="row">
-              <div className="column">
-                1. What are the tile precedence? <br/>
-                <center><img src={FAQ1} alt="Zac Efron Meme" height={250} width={300} /></center>
-              </div>
-              <div className="column">
-                2. How do I know if it's already the end tile? <br/>
-                <center><img src={FAQ2} alt="Condescending Wonka Meme" height={250} width={250} /></center>
-              </div>
-              <div className="column">
-                3. Any clues? Tips and tricks? <br/>
-                <center><img src={FAQ3} alt="Condescending Wonka Meme" height={250} width={250} /></center>
-              </div>
-            </div>
+        <br/>
+        <div className="row">
+          <div className="column">
+            1. What are the tile precedence? <br/>
+            <center><img src={FAQ1} alt="Zac Efron Meme" height={250} width={300} /></center>
+          </div>
+          <div className="column">
+            2. How do I know if it's already the end tile? <br/>
+            <center><img src={FAQ2} alt="Condescending Wonka Meme" height={250} width={250} /></center>
+          </div>
+          <div className="column">
+            **Click <button onClick={fetch_data}> here </button> to see ur daily meme** <br/>
+            <img src={url} alt="daily meme" height={250} width={250} />
+          </div>
+        </div>
     </div>
   );
 }
